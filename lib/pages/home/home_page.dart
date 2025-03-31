e3aba7a5f9f3ab8c541b3c17ed90f9a0339bcc19
Jose <161727897+joseaej@users.noreply.github.com>
2025-03-31 12:24:49 +0200

    fixed bugs

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:dietify/models/providers/goal_provider.dart';
 import 'package:dietify/models/providers/profile_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
 
 import '../../utils/theme.dart';
 
@@ -25,13 +26,98 @@ class _HomePageState extends State<HomePage> {
             ? profileProvider.profile!.username!
             : ""),
       ),
-      body: _buildOptionCard(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildCaloriesCard(),
+            _buildWaterCard(),
+          ],
+        ),
+      ),
     );
   }
 
-  Widget _buildOptionCard() {
+  Widget _buildCaloriesCard() {
     return Container(
-      height: 150.0,
+      height: 25.h,
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+        gradient: LinearGradient(
+          colors: [lightOrange, orange],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black38,
+            blurRadius: 8.0,
+            offset: const Offset(4, 4),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            /*
+            CircleAvatar(
+              radius: 35.0,
+              child: Icon(
+                Icons.water_drop_outlined,
+                size: 30,
+                color: Colors.black,
+              ),
+            ),*/
+            const SizedBox(width: 16.0),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Calories Tracker',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 6.0),
+                  Text(
+                    (goalProvider.goal != null && profileProvider.profile!=null)
+                        ? '${goalProvider.goal!.currentCalories} kcal  /  ${goalProvider.goal!.getTotalCalories(profileProvider.profile!.sex??"male", profileProvider.profile!.weight!, profileProvider.profile!.height!, profileProvider.profile!.age!)} kcal'
+                        : "",
+                    style: TextStyle(fontSize: 14.0, color: Colors.white70),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  )
+                ],
+              ),
+            ),
+            Consumer<GoalProvider>(
+              builder: (context, goalProvider, child) {
+                return Text(
+                  '${goalProvider.getWaterPercent()}%',
+                  style: TextStyle(
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWaterCard() {
+    return Container(
+      height: 25.h,
       margin: const EdgeInsets.all(10.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15.0),
