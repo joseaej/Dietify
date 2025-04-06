0f3106410c0cd73ce0d7cfa7a4f00ecfb3526b7d
Jose <armandoespi25@gmail.com>
2025-04-06 15:22:29 +0200

    TODO: fix workout profile

--- lib/models/repository/workout_repository.dart
+++ lib/models/repository/workout_repository.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
+import 'package:dietify/models/profile.dart';
 import 'package:dietify/models/workout.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class WorkoutRepository with ChangeNotifier {
   List<Workout> wokouts = List.empty(growable: true);
+  List<Workout> profileWorkout = List.empty(growable: true);
   final SupabaseClient _supabase = Supabase.instance.client;
 
   Future<List<Workout>> getAllWorkouts() async {
@@ -16,14 +20,35 @@ class WorkoutRepository with ChangeNotifier {
         await _supabase.from("workout").select().eq("id", id).single());
   }
 
-Future<Workout> getRandomWorkouts() async {
-  final map = await _supabase.rpc("getrandomworkout");
-  final workout = map.first as Map<String, dynamic>;
-  return Workout.fromMap(workout['result_json']);
-}
+  Future<Workout> getRandomWorkouts() async {
+    final map = await _supabase.rpc("getrandomworkout");
+    final workout = map.first as Map<String, dynamic>;
+    return Workout.fromMap(workout['result_json']);
+  }
 
   void insertWorkoutToSupabase(Workout workout) async {
     await _supabase.from("workout").insert(workout.toMap());
     notifyListeners();
   }
+
+  Future<void> saveWorkoutToProfile(Workout wokout, Profile profile) async {
+    final profileID = _supabase
+        .from("profile")
+        .select("id")
+        .eq("email", profile.email!)
+        .single();
+    print(jsonEncode(profileID.toString()));
+    _supabase.from("profile_workout").insert({
+      'profile_id': profileID,
+      'workout_id': wokout.id,
+    });
+  }
+  /*
+  Future<void> getAllWorkoutsToProfile(Profile profile) async {
+    if (profile.email!=null) {
+      final idProfile = await _supabase.from("profile").select("id").eq("email", profile.email!);
+      print(idProfile);
+      _supabase.from("profile_workout").select().eq(, value)
+    }
+  }*/
 }
