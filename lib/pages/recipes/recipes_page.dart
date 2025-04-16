621439610f95cd44be03a5912cfa78f07b84c25c
Jose <armandoespi25@gmail.com>
2025-04-16 14:38:39 +0200

    fixed bugs

--- lib/pages/recipes/recipes_page.dart
+++ lib/pages/recipes/recipes_page.dart
@@ -61,3 +61,20 @@ FutureBuilder(
 
 
 */ 
+import 'package:flutter/material.dart';
+
+class RecipesPage extends StatefulWidget {
+  const RecipesPage({super.key});
+
+  @override
+  State<RecipesPage> createState() => _RecipesPageState();
+}
+
+class _RecipesPageState extends State<RecipesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
